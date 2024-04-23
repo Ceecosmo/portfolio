ed7688dd9dc04945b3739a937aab5ebb0a91488c
Ceecosmo <153816310+Ceecosmo@users.noreply.github.com>
2024-04-23 15:51:47 +0100

    Update paymenthome.jsx

--- src/components/paymenthome.jsx
+++ src/components/paymenthome.jsx
@@ -7,7 +7,7 @@ function paymenthome() {
       <br></br> <br></br> <br></br><br></br> <br></br> <br></br>
       <div className='text-center'>
     <h7 className="display-6 fw-bold 1h-1 mb-3 ">Welcome to Priests' Welfare Home Awka, Nigeria.</h7>
-    <br></br>
+    <br></br><br></br>
 <h5 className="fw-bold text-center">WE HONOUR YOUR PRESENCE</h5>
 <br></br> <br></br> 
 
